5ecc3aff96899a1cd7bb5afb7a5ed5fb71769e00
jinxu-ai <taojinxu7@gmail.com>
2025-09-02 00:05:09 +0800

    env & API test | main, deepseek_chain file created

--- test_env.py
+++ test_env.py
@@ -1,19 +1,4 @@
-# # Please install OpenAI SDK first: `pip3 install openai`
 
-# from openai import OpenAI
-
-# client = OpenAI(api_key="<DeepSeek API Key>", base_url="https://api.deepseek.com")
-
-# response = client.chat.completions.create(
-#     model="deepseek-chat",
-#     messages=[
-#         {"role": "system", "content": "You are a helpful assistant"},
-#         {"role": "user", "content": "Hello"},
-#     ],
-#     stream=False
-# )
-
-# print(response.choices[0].message.content)
 
 
 
@@ -25,3 +10,27 @@ load_dotenv()
 
 print("OPENAI-API-Key: ", os.getenv("OPENAI_API_KEY"))
 print("DEEPSEEK-API-Key: ", os.getenv("DEEPSEEK_API_KEY"))
+
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph
+
+print("LangChain & LangGraph imported successfully!")
+
+
+
+# Please install OpenAI SDK first: `pip3 install openai`
+
+from openai import OpenAI
+
+client = OpenAI(api_key=os.getenv("DEEPSEEK_API_KEY"), base_url="https://api.deepseek.com")
+
+response = client.chat.completions.create(
+    model="deepseek-chat",
+    messages=[
+        {"role": "system", "content": "You are a helpful assistant"},
+        {"role": "user", "content": "Hello, Who are you?"},
+    ],
+    stream=False
+)
+
+print(response.choices[0].message.content)
